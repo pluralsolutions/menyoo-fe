ff388499a0b0eae30bc1c3e039ad54b2c170101f
logonrm <logonrm@fiap.com.br>
2017-08-14 20:20:06 -0300

    Adding images on header and footer into authenticator component

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="authentication-container">
-    <div class="authenticators">
-      <router-link class="authenticator-item facebook" to="/home">Entre com Facebook</router-link>
-      <router-link class="authenticator-item google" to="/home">Entre com Google</router-link>
-      <router-link class="authenticator-item menyoo-account" to="/home">Entre com Email</router-link>
+    <div class="elements">
+      <div class="plate"></div>
+      <div class="authenticators">
+        <router-link class="authenticator-item facebook" to="/home">Entre com Facebook</router-link>
+        <router-link class="authenticator-item google" to="/home">Entre com Google</router-link>
+        <router-link class="authenticator-item menyoo-account" to="/home">Entre com Email</router-link>
+      </div>
+      <div class="menyoo-slogan"></div>
     </div>
   </div>
 </template>
