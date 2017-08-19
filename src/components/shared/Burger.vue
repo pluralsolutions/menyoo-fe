5ad2550f4e38ff1d1ae7602c538b03b44faab7bc
lucasgomide <lucaslg200@gmail.com>
2017-08-19 00:25:55 -0300

    Refactoring Menu component

--- src/components/shared/Burger.vue
+++ src/components/shared/Burger.vue
@@ -1,38 +1,17 @@
 <template>
-  <div @click="open" class="burger">
-    <div :class="{'container': true, 'close': isOpen}">
-      <div class="bar1"></div><div class="bar2"></div><div class="bar3"></div>
-    </div>
+  <div @click="toogle" class="burger-container">
+    <div class="bar-item" />
+    <div class="bar-item" />
+    <div class="bar-item" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'burger',
-  props: {
-    value: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
-  watch: {
-    value: function val() {
-      this.isOpen = this.value;
-    },
-  },
   methods: {
-    open: function bc() {
-      this.isOpen = !this.isOpen;
-      this.$emit('input', this.isOpen);
-    },
-    close: function close() {
-      this.isOpen = false;
-      this.$emit('input', this.isOpen);
+    toogle: function toogle() {
+      this.$emit('toogle');
     },
   },
 };
