5cca94679d938751c0ac98c7bff9f10fef2e1007
lucasgomide <lucaslg200@gmail.com>
2017-08-19 15:32:47 -0300

    Merge Burger into MenuNav component and keep ease UI

--- src/components/shared/Menu.vue
+++ src/components/shared/Menu.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <Burger @toogle="toogleMenu"></Burger>
     <MenuNav @toogle="toogleMenu" v-model="menuIsOpen"></MenuNav>
   </div>
 </template>
 <script>
 
-import Burger from '@/components/shared/Burger';
 import MenuNav from '@/components/shared/MenuNav';
 
 export default {
   components: {
-    Burger,
     MenuNav,
   },
   data() {
--- src/components/shared/MenuNav.vue
+++ src/components/shared/MenuNav.vue
@@ -1,31 +1,41 @@
 <template>
-  <nav :class="{'show-menu': isOpen}" class="menu-container" id="menu-content">
-    <div class="menu">
-      <a @click="toogle" class="close">X</a>
-      <div class="profile">
-        <div class="user-img">
-          <PhotoCircle :src="user.urlPhoto" :alt="`${user.name}`" size="69"></PhotoCircle>
-        </div>
-        <div class="user-name">
-          Gabriel Lucas
-        </div>
-      </div>
-      <ul class="menu-nav">
-        <li class="menu-item edit-profile">
-          <router-link v-on:click.native="toogle" to="/perfil">Editar perfil</router-link>
-        </li>
-        <li class="menu-item tracking-orders">
-          <router-link v-on:click.native="toogle" to="/pedidos/acompanhar">Acompanhar pedido</router-link>
-        </li>
-      </ul>
-
-      <ul class="menu-secondary">
-        <li class="menu-item logout">
-          <router-link to="/logout">Sair</router-link>
-        </li>
-      </ul>
+  <div>
+    <div @click="show = !show" class="burger-container">
+      <div class="bar-item" />
+      <div class="bar-item" />
+      <div class="bar-item" />
     </div>
-  </nav>
+
+    <transition name="fade">
+      <nav v-if="show" @click="show = !show" class="menu-container">
+        <div class="menu">
+          <div class="profile">
+            <div class="user-img">
+              <PhotoCircle :src="user.urlPhoto" :alt="`${user.name}`" size="69"></PhotoCircle>
+            </div>
+            <div class="user-name">
+              Gabriel Lucas
+            </div>
+          </div>
+
+          <ul class="menu-nav">
+            <li class="menu-item edit-profile">
+              <router-link to="/perfil">Editar perfil</router-link>
+            </li>
+            <li class="menu-item tracking-orders">
+              <router-link to="/pedidos/acompanhar">Acompanhar pedido</router-link>
+            </li>
+          </ul>
+
+          <ul class="menu-secondary">
+            <li class="menu-item logout">
+              <router-link to="/logout">Sair</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </transition>
+  </div>
 </template>
 <script>
 
@@ -37,28 +47,11 @@ export default {
     PhotoCircle,
     User,
   },
-  props: {
-    value: {
-      type: Boolean,
-      default: false,
-    },
-  },
   data() {
     return {
-      isOpen: false,
+      show: false,
       user: new User('Gabriel Toledo', '/static/img/user.jpg'),
     };
-  },
-  watch: {
-    value: function val() {
-      this.isOpen = this.value;
-    },
-  },
-  methods: {
-    toogle() {
-      this.$emit('input', this.isOpen);
-      this.$emit('toogle');
-    },
   },
 };
 </script>
