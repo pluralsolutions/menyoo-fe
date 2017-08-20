c10051b10a360ed468c914d02b86369924b2174b
lucasgomide <lucaslg200@gmail.com>
2017-08-20 00:28:32 -0300

    Check if user is logged in on Header

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -3,9 +3,9 @@
     <div class="elements">
       <div class="plate"></div>
       <div class="authenticators">
-        <router-link class="authenticator-item facebook" to="/home">Entre com Facebook</router-link>
-        <router-link class="authenticator-item google" to="/home">Entre com Google</router-link>
-        <router-link class="authenticator-item menyoo-account" to="/home">Entre com Email</router-link>
+        <div @click="signInFacebook" class="authenticator-item facebook">Entre com Facebook</div>
+        <div @click="signInGoogle" class="authenticator-item google">Entre com Google</div>
+        <div @click="signInEmail" class="authenticator-item menyoo-account" to="/home">Entre com Email</div>
       </div>
       <div class="menyoo-slogan"></div>
     </div>
@@ -14,10 +14,19 @@
 
 <script>
 export default {
-  name: 'authentication',
-  data() {
-    return {
-    };
+  methods: {
+    signInFacebook: function signInFacebook() {
+      this.$store.dispatch('signInFacebook');
+      this.$router.push('home');
+    },
+    signInGoogle: function signInGoogle() {
+      this.$store.dispatch('signInGoogle');
+      this.$router.push('home');
+    },
+    signInEmail: function signInEmail() {
+      this.$store.dispatch('signInEmail');
+      this.$router.push('home');
+    },
   },
 };
 </script>
