fad62603586c1bf5c1bfd406ec7474cf3a32ed30
Gabriel Ribeiro <gabrielltr84@gmail.com>
2017-08-23 19:09:37 -0300

    Creating Awaiting order

---
+++ src/components/AwaitingOrder.vue
@@ -0,0 +1,46 @@
+﻿<template>
+  <div>
+    <div><div @click="mais()">++++</div><div @click="menos()">----</div>{{step}}</div>
+    <div :class="{awaitorder:1, step1:1, active: step>=1}"></div>
+    <div :class="{awaitorder:1, step2:1, active: step>=2}"></div>
+    <div :class="{awaitorder:1, step3:1, active: step>=3}"></div>
+  </div>
+</template>
+
+<script>
+import RestaurantHero from '@/components/shared/RestaurantHero';
+
+export default {
+  name: 'AwaitingOrder',
+  components: {
+    RestaurantHero,
+  },
+  data() {
+    return {
+      // step: 1,
+    };
+  },
+  methods: {
+    mais() {
+      console.log(this.$store);
+      this.$store.state.awaitingorder.step = 2;
+    },
+    menos() {
+      console.log(this.$store);
+      this.$store.state.awaitingorder.step = 3;
+    },
+    refresh() {
+      // TODO: buscar atualização
+    },
+  },
+  computed: {
+    step() {
+      return this.$store.state.awaitingorder.step;
+    },
+  },
+};
+</script>
+
+<style>
+  @import '../assets/styles/awaitingorder.css';
+</style>
