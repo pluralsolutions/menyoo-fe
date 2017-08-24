ae5fed321d94482351ebff660c227c402216527e
lucasgomide <lucaslg200@gmail.com>
2017-08-24 17:39:10 -0300

    Creating ProductInfo component to reuse

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -3,52 +3,38 @@
     <NavigationBar type="product-desc"/>
     <div class="product-galery" />  <!-- Should be a Galery Component-->
     <div class="product-info">
-      <div class="product-header">
-        <span class="product-price-unit">Preço unitário</span>
-        <span class="product-price">R$ 43,00</span>
-        <div class="product-evaluations" v-if="score >= 20 ">
-          <span class="star-neutral"></span>
-          <span class="star-active" v-bind:style="{width: score + '%'}"></span>
-        </div>
-      </div>
-      <div class="product-content">
-        <div class="product-title">Pizza Muçarela grande</div>
-        <div class="product-desc">
-          PIZZA COM MASSA TRANDICIONAL, RECHEIO DE MOLHO DE
-          TOMATE FRESCO, CEBOLA DOURADA E MUSSARELA SADIA.
-        </div>
-      </div>
-      <div class="product-add-remove">
-        <div class="add" />
-        <div class="current-quantity">01</div>
-        <div class="remove" />
-      </div>
+      <ProductInfo />
       <div class="custom-ingredients">
         <span class="title">Personalize seus ingredientes</span>
         <ul class="ingredients-list">
           <li>
             <span class="ingredientes-section">Padrões deste prato</span>
             <ul>
-              <li>Padrão 1</li>
-              <li>Padrão 2</li>
-              <li>Padrão 3</li>
+              <li>
+                <span @click="toogleIt" class="checkbox checked" />
+                <span class="ingredient-name">Tomate</span>
+              </li>
+              <li>
+                <span @click="toogleIt" class="checkbox checked" />
+                <span class="ingredient-name">Azeite</span>
+              </li>
             </ul>
           </li>
           <li>
             <span class="ingredientes-section">Queijos</span>
             <ul>
               <li>
-                <span class="checkbox checked" />
+                <span @click="toogleIt" class="checkbox checked" />
                 <span class="ingredient-name">Queijo Gouda</span>
                 <span class="additional-cost">+R$ 1,40</span>
               </li>
               <li>
-                <span class="checkbox" />
+                <span @click="toogleIt" class="checkbox" />
                 <span class="ingredient-name">Queijo Gorgonzola</span>
                 <span class="additional-cost">+R$ 1,40</span>
               </li>
               <li>
-                <span class="checkbox checked" />
+                <span @click="toogleIt" class="checkbox checked" />
                 <span class="ingredient-name">Queijo Emental</span>
                 <span class="additional-cost">+R$ 1,40</span>
               </li>
@@ -62,15 +48,17 @@
 
 <script>
 import NavigationBar from '@/components/shared/NavigationBar';
+import ProductInfo from '@/components/shared/ProductInfo';
 
 export default {
   components: {
     NavigationBar,
+    ProductInfo,
   },
-  data() {
-    return {
-      score: 20,
-    };
+  methods: {
+    toogleIt: function toogleIt(event) {
+      event.target.classList.toggle('checked');
+    },
   },
 };
 </script>
---
+++ src/components/shared/ProductInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="product-header">
+      <span class="product-price-unit">Preço unitário</span>
+      <span class="product-price">R$ 43,00</span>
+      <div class="product-evaluations" v-if="score >= 20 ">
+        <span class="star-neutral"></span>
+        <span class="star-active" v-bind:style="{width: score + '%'}"></span>
+      </div>
+      </div>
+      <div class="product-content">
+        <div class="product-title">Pizza Muçarela grande</div>
+        <div class="product-desc">
+          PIZZA COM MASSA TRANDICIONAL, RECHEIO DE MOLHO DE
+          TOMATE FRESCO, CEBOLA DOURADA E MUSSARELA SADIA.
+        </div>
+      </div>
+
+      <div class="product-add-remove">
+        <div class="add" />
+        <div class="current-quantity">01</div>
+        <div class="remove" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      score: 20,
+    };
+  },
+};
+</script>
+
+<style>
+  @import '../../assets/styles/shared/product-info.css';
+</style>
