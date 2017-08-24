042d06df920f6dc800ffe9ad1873bda314fe8863
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-08-24 00:50:55 -0300

    add home route to menu & order start button

--- src/components/shared/MenuNav.vue
+++ src/components/shared/MenuNav.vue
@@ -19,6 +19,9 @@
           </div>
 
           <ul class="menu-nav">
+            <li class="menu-item home">
+              <router-link to="/home">Início</router-link>
+            </li>
             <li class="menu-item edit-profile">
               <router-link to="/perfil">Editar perfil</router-link>
             </li>
