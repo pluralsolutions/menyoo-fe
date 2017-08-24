88eb0285a1bc07652fcc2ff81013b224dbd1688e
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-08-24 00:34:46 -0300

    fix vuex getters & actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './modules/authentication';
-import awaitingorder from './modules/awaitingorder';
+import order from './modules/order';
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
+
 export default new Vuex.Store({
   modules: {
     auth,
-    awaitingorder,
+    order,
   },
   strict: debug,
 });
