dc086d03467534c04378547e8a1845852ffb027e
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-08-24 23:49:48 -0300

    fix gray color && button pay

--- src/components/AwaitingOrder.vue
+++ src/components/AwaitingOrder.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
+    <NavigationBar/>
     <div class="awaitorder">
       <div :class="{active: step>=1}"><img src="../assets/images/step1.svg"/><span>Seu pedido já foi para o balcão do chefe</span></div>
       <div :class="{active: step>=2}"><img src="../assets/images/step2.svg"/><span>Seu pedido está sendo preparado</span></div>
       <div :class="{active: step>=3}"><img src="../assets/images/step3.svg"/><span>Seu pedido está a caminho da sua mesa</span></div>
     </div>
-	<button>PAGAR</button>
+    <button v-if="step>=3">PAGAR</button>
     <div><button @click="mais()">+++</button> {{ step }} <button @click="menos()">----</button> </div>
   </div>
 </template>
 
 <script>
-import RestaurantHero from '@/components/shared/RestaurantHero';
+import NavigationBar from '@/components/shared/NavigationBar';
 
 export default {
   name: 'AwaitingOrder',
   components: {
-    RestaurantHero,
+    NavigationBar,
   },
   methods: {
     mais() {
