00bf46ccea67b72e859ad47ee3eac7e31f36c0ca
lucasgomide <lucaslg200@gmail.com>
2017-08-27 01:49:30 -0300

    Using css module on navigatin bar

--- src/components/shared/NavigationBar.vue
+++ src/components/shared/NavigationBar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="navigation-bar-content">
-    <div class="navigation-fixed">
-      <div @click="back" class="back">Voltar</div>
-      <div v-bind:class="[{type_icon: true}, type]" v-if="type"><slot/></div>
+  <div :class="$style['navigation-bar-content']">
+    <div :class="$style['navigation-fixed']">
+      <div @click="back" :class="$style.back">Voltar</div>
+      <div :class="$style.type_icon" v-if="type"><slot/></div>
     </div>
   </div>
 </template>
@@ -18,6 +18,51 @@ export default {
 };
 </script>
 
-<style>
-  @import '../../assets/styles/shared/navigation-bar.css';
+<style module>
+  .navigation-bar-content {
+    height: 60px;
+  }
+
+  .navigation-fixed {
+    font-size: 14px;
+    text-transform: uppercase;
+    color: #fff;
+    flex-direction: row;
+    display: flex;
+    align-items: center;
+    background-color: #be1622;
+    height: 60px;
+  }
+
+  .back, .page-title {
+    position: relative;
+  }
+
+  .back{
+    width: 60%;
+    text-align: left;
+    padding-left: 40px;
+    cursor: pointer;
+  }
+
+  .back:before{
+    content: '';
+    width: 22px;
+    height: 100%;
+    background: url('../../assets/images/back_icon.svg') no-repeat center center;
+    position: absolute;
+    left: 10px;
+  }
+
+  .page-title{
+    width: 40%;
+  }
+
+  .page-title:before{
+    content: '';
+    width: 22px;
+    height: 30px;
+    position: absolute;
+    left: 0;
+  }
 </style>
