4e6dc0b53adae5a2b4f0019d8bfec7d5f4f6c468
lucasgomide <lucaslg200@gmail.com>
2017-08-27 01:11:07 -0300

    Using css-modules to compose styles

--- src/components/shared/Button.vue
+++ src/components/shared/Button.vue
@@ -11,7 +11,7 @@ export default {
   },
   computed: {
     btnClass: function btnClass() {
-      return `button ${this.type} ${this.size} ${this.additionalClass}`;
+      return `${this.$style.button} ${this.$style[this.type]} ${this.$style[this.size]} ${this.additionalClass}`;
     },
   },
   data() {
@@ -21,30 +21,30 @@ export default {
 };
 </script>
 
-<style scoped>
-.button {
-  background-color: #4a79d1;
-  padding: 14px;
-  font-size: 16px;
-  margin-top: 20px;
-  border: none;
-  color: #fff;
-  text-transform: uppercase;
-  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.38), inset 0 0 6.8px 3.2px rgba(0, 0, 0, 0.06);
-}
-.button.primary{
-  background-color: #4a79d1;
-}
-.button.secondary{
-  background-color: #743;
-}
-.button.large{
-  width: 80%;
-}
-.button.medium{
-  width: 45%;
-}
-.button.small{
-  width: 24%;
-}
+<style module>
+  .button {
+    background-color: #4a79d1;
+    padding: 14px;
+    font-size: 16px;
+    margin-top: 20px;
+    border: none;
+    color: #fff;
+    text-transform: uppercase;
+    box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.38), inset 0 0 6.8px 3.2px rgba(0, 0, 0, 0.06);
+  }
+  .primary{
+    background-color: #4a79d1;
+  }
+  .secondary{
+    background-color: #743;
+  }
+  .large{
+    width: 80%;
+  }
+  .medium{
+    width: 45%;
+  }
+  .small{
+    width: 24%;
+  }
 </style>
