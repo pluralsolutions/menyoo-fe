f266277b7778868994db7216aeeb8c155ec9c030
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-08-28 01:09:32 -0300

    added Order Evaluation Push notification

--- src/components/shared/OrderEvaluationPush.vue
+++ src/components/shared/OrderEvaluationPush.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="order-evaluation-push">
-    <div class="x">X</div>
-    <div class="row">
-      <div class="label">Terminou de comer?<br>Avalie seu prato</div>
+  <transition name="fade">
+    <div class="order-evaluation-push" v-show="isshowing" @click.prevent="isshowing=true">
+      <div class="x" @click="close">x</div>
+      <div class="row-label">
+        <div class="label">Terminou de comer?<br>Avalie seu prato...</div>
+      </div>
+      <div class="row-star">
+        <div :class="['star', {'star-active': score>=i}]" v-for="i in 5" :key="i" :value="i" @click="score=(score==i)?0:i" />
+      </div>
+      <div class="row-btn">
+        <ButtonComponent size="large" :disabled="score == 0" :onClick="close">Avalie</ButtonComponent>
+      </div>
     </div>
-      <ProductEvaluation style="text-align:center" :score="10"></ProductEvaluation>
-    <div class="row">      
-      <ButtonComponent size="large">Avalie</ButtonComponent>
-    </div>
-  </div>
+  </transition>
 </template>
 <script>
 export default {
   components: {
     ButtonComponent: () => import('@/components/shared/Button'),
-    ProductEvaluation: () => import('@/components/shared/ProductEvaluation'),
+  },
+  data() {
+    return {
+      isshowing: true,
+      score: 0,
+    };
   },
   methods: {
+    close: function c() {
+      this.isshowing = false;
+    },
   },
 };
 </script>
@@ -24,25 +36,53 @@ export default {
 .order-evaluation-push {
   position: fixed;
   width: 100%;
-  /* 25% of visual height */
-  height: 25vh; 
+  height: 210px;
   bottom: 0px;
   background-color: #be1622;
   color: #fff;
-  display: flex;
+  display: relative;
   flex-direction: column;
+  font-size: 25.6px;
+  font-weight: 300;
 }
 
 .order-evaluation-push .x {
-  position: absolute;
-  right: 0px;
-  padding: 10px;
+  float: right;
+  height: 20px;
+  width: 20px;
 }
 
-.order-evaluation-push .row {
+.order-evaluation-push .row-label,
+.order-evaluation-push .row-star {
+  padding: 10px 35px;
+}
+.order-evaluation-push .row-star,
+.order-evaluation-push .row-btn {
+  text-align: center;
 }
 
-.order-evaluation-push .label {
-  padding: 20px 35px;
+.order-evaluation-push .row-btn {
+  /* text-align: center; */
+  width: 80%;
+  flex-direction: column;
+  margin: auto auto;
+}
+
+.order-evaluation-push .star {
+  background: url('../../assets/images/product-detail/star-active.svg');
+  background-size: 27px;
+  width: 27px;
+  height: 27px;
+  margin: 0px 5px;
+  border: 1px solid green;
+  opacity: 0.3;
+  display: inline-flex;
+}
+
+.order-evaluation-push .star-active {
+  opacity: 1;
+}
+
+@media (min-width: 600px) {
 }
 </style>
