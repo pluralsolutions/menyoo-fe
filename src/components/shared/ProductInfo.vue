3e39a74cca08d2242d5d6876f4976e18c2d81411
lucasgomide <lucaslg200@gmail.com>
2017-08-31 17:43:00 -0300

    Add order to store
    Adding product, product quantity and ingredients into order object

--- src/components/shared/ProductInfo.vue
+++ src/components/shared/ProductInfo.vue
@@ -9,46 +9,21 @@
       <div class="product-title">{{product.title}}</div>
       <div class="product-desc">{{product.description}}</div>
     </div>
-    <div class="summary-items">
-      <Counter :plusCallback="addProduct" :minusCallback="removeProduct" v-model:value="count"/>
-      <div class="current-price">
-        R$ {{ itemsPrice | currency }}
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
 import Product from '@/domain/Product';
-import Counter from '@/components/shared/Counter';
 import ProductEvaluation from '@/components/shared/ProductEvaluation';
 
 export default {
-  data() {
-    return {
-      count: 1,
-      itemsPrice: this.product.unitPrice,
-    };
-  },
   components: {
-    Counter,
     ProductEvaluation,
   },
   props: {
     product: {
       type: Product,
       required: true,
-    },
-  },
-  methods: {
-    addProduct: function addProduct() {
-      this.itemsPrice = this.product.unitPrice * this.count;
-    },
-    removeProduct: function removeProduct() {
-      if (this.count > 1) {
-        this.count -= 1;
-        this.itemsPrice = this.product.unitPrice * this.count;
-      }
     },
   },
 };
