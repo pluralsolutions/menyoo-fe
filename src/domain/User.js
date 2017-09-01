551c80aeceedce4b7f4c7b22645f08d4fd5a07f6
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-09-01 01:19:41 -0300

    sample User model

--- src/domain/User.js
+++ src/domain/User.js
@@ -1,7 +1,12 @@
 export default class User {
 
-  constructor(name = '', urlPhoto = '') {
+  constructor(token, name = '', urlPhoto = '') {
+    this.token = token;
     this.name = name;
     this.urlPhoto = urlPhoto;
+  }
+
+  static sample() {
+    return new User('123345', 'Gabriel Lucas de Toledo Toledo', 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg');
   }
 }
