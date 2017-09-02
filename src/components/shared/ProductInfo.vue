f58b237391b2a9a242f4c0c7bf077593bdd6a7a3
lucasgomide <lucaslg200@gmail.com>
2017-09-01 23:12:24 -0300

    Calculating each ingredients price to total order price
    Fix counter calculation

--- src/components/shared/ProductInfo.vue
+++ src/components/shared/ProductInfo.vue
@@ -9,22 +9,58 @@
       <div class="product-title">{{product.title}}</div>
       <div class="product-desc">{{product.description}}</div>
     </div>
+    <div class="summary-items">
+      <Counter :plusCallback="updatePrice" :minusCallback="updatePrice" v-model:value="productQuantity"/>
+      <div class="current-price">
+        R$ {{ itemsPrice | currency }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Product from '@/domain/Product';
 import ProductEvaluation from '@/components/shared/ProductEvaluation';
+import Counter from '@/components/shared/Counter';
 
 export default {
   components: {
     ProductEvaluation,
+    Counter,
   },
   props: {
+    value: {
+      type: Number,
+      default: 0,
+    },
     product: {
       type: Product,
       required: true,
     },
+    additionalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      productQuantity: 1,
+      itemsPrice: 0,
+    };
+  },
+  watch: {
+    additionalPrice: function additionalPrice() {
+      this.updatePrice();
+    },
+  },
+  methods: {
+    updatePrice: function updatePrice() {
+      this.$emit('input', this.productQuantity);
+      this.itemsPrice = (this.product.unitPrice * this.productQuantity) + this.additionalPrice;
+    },
+  },
+  created() {
+    this.updatePrice();
   },
 };
 </script>
