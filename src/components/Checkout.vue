ccc422eb5382b4236f185a58e63514df7dd2377f
logonrm <logonrm@fiap.com.br>
2017-09-05 20:40:03 -0300

    Setup layout

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -2,10 +2,17 @@
   <div class="checkout-container">
     <NavigationBar>Checkout</NavigationBar>
     <div class="products">
-      <ul>
+      <ul class="order-items">
         <li v-for="order in orders">
-          <ProductInfo :product="order.product" :noSummary="true" :noRating="true" />
-          <span class="ingredient-item" v-for="ingredient in order.ingredients"> +{{ingredient.name}}</span>
+          <span class="product-quantity">
+            {{order.productQuantity}} unid
+          </span>
+          <div>
+            <ProductInfo :product="order.product" :noSummary="true" :noRating="true" />
+            <span class="ingredient-item" v-for="ingredient in order.ingredients"> +{{ingredient.name}}</span>
+            <Counter :plusCallback="addProductIntoOrder.bind(null, order.product)" 
+                     :minusCallback="removeProductIntoOrder.bind(null, order.product)" />
+          </div>
         </li>
       </ul>
     </div>
@@ -15,6 +22,7 @@
 <script>
 import NavigationBar from '@/components/shared/NavigationBar';
 import ProductInfo from '@/components/shared/ProductInfo';
+import Counter from '@/components/shared/Counter';
 
 import { mapGetters } from 'vuex';
 
@@ -22,6 +30,15 @@ export default {
   components: {
     NavigationBar,
     ProductInfo,
+    Counter,
+  },
+  methods: {
+    addProductIntoOrder(product) {
+      console.log(product);
+    },
+    removeProductIntoOrder(product) {
+      console.log(product);
+    },
   },
   computed: {
     ...mapGetters([
