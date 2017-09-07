1f72b2686478c160fad43992fb8ccb98e534dc68
lucasgomide <lucaslg200@gmail.com>
2017-09-07 15:20:14 -0300

    Refactoring "Add produto to order"

--- src/domain/Order.js
+++ src/domain/Order.js
@@ -1,8 +1,18 @@
 export default class Order {
-  constructor({ product, productQuantity, ingredients, totalValue }) {
-    this.product = product;
-    this.productQuantity = productQuantity;
-    this.ingredients = ingredients;
+  constructor({ products = [], totalValue = 0 }) {
+    this.products = products;
     this.totalValue = totalValue;
+  }
+
+  addProduct(productOrder) {
+    const productIndex = this.products.findIndex(p => productOrder.equalTo(p));
+    if (productIndex >= 0) {
+      this.products[productIndex].productQuantity += productOrder.productQuantity;
+    } else {
+      this.products.push(productOrder);
+    }
+
+    this.totalValue += productOrder.productOrderIngredients.sumAdditionalPrice() +
+                        (productOrder.productQuantity * productOrder.product.unitPrice);
   }
 }
--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -1,6 +1,8 @@
+import Order from '@/domain/Order';
+
 import {
   ORDER_STEP_CHANGE,
-  ADD_ITEM_TO_ORDER,
+  ADD_PRODUCT_TO_ORDER,
   DUPLICATE_ITEM_ON_ORDER,
   REMOVE_ITEM_FROM_ORDER,
 } from '../mutation-types';
@@ -8,6 +10,7 @@ import {
 const getters = {
   orderStep: state => (state.orderStep),
   orders: state => (state.orders),
+  order: state => (state.order),
 };
 
 const actions = {
@@ -16,8 +19,8 @@ const actions = {
     newstep += step || 1;
     commit.commit(ORDER_STEP_CHANGE, { newstep });
   },
-  addItemToOrder(state, order) {
-    state.commit(ADD_ITEM_TO_ORDER, order);
+  addProductToOrder(state, orderProduct) {
+    state.commit(ADD_PRODUCT_TO_ORDER, orderProduct);
   },
   duplicateItemOnOrder(state, orderIndex) {
     state.commit(DUPLICATE_ITEM_ON_ORDER, orderIndex);
@@ -31,8 +34,11 @@ const mutations = {
   [ORDER_STEP_CHANGE](state, { newstep }) {
     state.orderStep = newstep;
   },
-  [ADD_ITEM_TO_ORDER](state, order) {
-    state.orders.push(order);
+  [ADD_PRODUCT_TO_ORDER](state, orderProduct) {
+    const order = state.order || new Order({});
+    order.addProduct(orderProduct);
+
+    state.order = order;
   },
   [DUPLICATE_ITEM_ON_ORDER](state, orderIndex) {
     const order = state.orders[orderIndex];
@@ -52,6 +58,7 @@ const mutations = {
 
 const state = {
   orderStep: 0,
+  order: null,
   orders: [],
 };
 
