8a42462d96c80cf136185a7f1cdd9e772c8b23e3
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-09-08 01:57:48 -0300

    add new component Currency

---
+++ src/components/shared/Currency.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :style="`width: ${width}px`" :class="$style.currency">{{ symbol }}
+    <span :class="$style[color]">
+      {{ value | currency }}
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: { default: 0 },
+    color: { default: 'black' },
+    width: { default: '80' },
+    symbol: { default: 'R$' },
+  },
+};
+</script>
+
+<style module>
+.currency{
+  font-size: 15.5px;  
+  font-weight: 400;
+  padding: 5px 0;
+}
+.black{
+  color: #000;  
+}
+.green{
+  color: #029016;  
+}
+</style>
