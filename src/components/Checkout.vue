de09b347cab005338c0e8f61dd584a2b2cabfd3e
lucasgomide <lucaslg200@gmail.com>
2017-09-09 14:36:42 -0300

    Creating preview selected products in order to re-use it

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -10,27 +10,10 @@
         <ButtonComponent :onClick="confirmed" size="medium" type="secondary">Sim</ButtonComponent>
         <ButtonComponent @click.native="showModal = false" size="medium" type="danger">Não</ButtonComponent>
       </p>
-    </modal>    
+    </modal>
     <NavigationBar type="checkout">Checkout</NavigationBar>
-    <div class="order-items">
-      <ul>
-        <li v-for="orderProduct in order.products" :key="orderProduct.product.id">
-          <span class="product-quantity">
-            {{orderProduct.productQuantity}} unid
-          </span>
-          <div>
-            <ProductInfo :product="orderProduct.product" :summary="false" :rating="false" />
-            <div class="ingredients-item">
-              <span v-for="ingredient in orderProduct.ingredients" :key="ingredient.name"> +{{ingredient.name}}</span>
-            </div>
-            <Counter :plusCallback="addProductToOrder.bind(null, orderProduct)"
-                     :minusCallback="removeProductIntoOrder.bind(null, orderProduct)"
-                     :showCounter="false"
-                     justifyContent="flex-end"
-                     />
-          </div>
-        </li>
-      </ul>
+    <div>
+      <PreviewSelectedProducts />
       <div class="order-total">
         <span class="desc">Total pedido</span>
         <span class="price">R$ {{order.totalValue | currency}}</span>
@@ -42,20 +25,15 @@
 
 <script>
 import NavigationBar from '@/components/shared/NavigationBar';
-import ProductInfo from '@/components/shared/ProductInfo';
-import Counter from '@/components/shared/Counter';
+import PreviewSelectedProducts from '@/components/shared/PreviewSelectedProducts';
 import ButtonComponent from '@/components/shared/Button';
-
-import ProductOrder from '@/domain/ProductOrder';
-import Product from '@/domain/Product';
 
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
     NavigationBar,
-    ProductInfo,
-    Counter,
+    PreviewSelectedProducts,
     ButtonComponent,
   },
   data() {
@@ -64,23 +42,6 @@ export default {
     };
   },
   methods: {
-    addProductToOrder(item) {
-      const productOrder = new ProductOrder({
-        product: item.product,
-        productOrderIngredients: item.ingredients,
-        productQuantity: 1,
-      });
-
-      this.$store.dispatch('addProductToOrder', productOrder);
-    },
-    removeProductIntoOrder(item) {
-      const productOrder = new ProductOrder({
-        product: new Product(item.product),
-        productOrderIngredients: item.ingredients,
-      });
-
-      this.$store.dispatch('removeProductFromOrder', { productOrder, quantity: 1 });
-    },
     redirectToRestaurant() {
       if (this.order === null) {
         this.$router.push('/restaurantes/bar-do-ze');
@@ -110,9 +71,7 @@ export default {
     background: url('../assets/images/logo.svg');
     border-bottom: solid #f00 2px;
   }
-  .ingredients-item {
-    margin-bottom: 10px;
-  }
+
   .body-modal {
     text-transform: uppercase;
     text-align: center;
@@ -126,16 +85,10 @@ export default {
     display: flex;
   }
 
-  .order-items {
-    margin-top: 30px;
-    padding: 0 20px;
-  }
-  .order-items li {
-    padding: 0;
-  }
   .order-total {
     display: flex;
     justify-content: space-between;
+    padding: 0 20px;
   }
 
   .order-total .desc{
