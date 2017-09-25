3f92e96e05ff441c5a51c49be7925633b027363d
lucasgomide <lucaslg200@gmail.com>
2017-09-24 21:56:54 -0300

    Adding item into product using API

--- src/lib/request.js
+++ src/lib/request.js
@@ -3,11 +3,19 @@ import Vue from 'vue';
 
 const get = url =>
     Vue.http.get(url).then(response => (
-      Promise.resolve(response.body)
+      response.json()
     ), () => {
       Promise.reject();
     });
 
+const post = (url, body) =>
+    Vue.http.post(url, body).then(response => (
+      Promise.resolve(response.body)
+    ), (err) => {
+      Promise.reject(err);
+    });
+
 export default {
   get,
+  post,
 };
