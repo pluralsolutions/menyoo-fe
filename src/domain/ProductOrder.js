5c9578a11711085daeae7765f728b4f54d102cd6
lucasgomide <lucaslg200@gmail.com>
2017-09-26 16:43:31 -0300

    Get score when created Review component

--- src/domain/ProductOrder.js
+++ src/domain/ProductOrder.js
@@ -1,15 +1,17 @@
 /* eslint-disable camelcase */
 import IngredientProductOrder from '@/domain/IngredientProductOrder';
 import Product from '@/domain/Product';
+import Evaluation from '@/domain/Evaluation';
 
 export default class ProductOrder {
-  constructor({ id, product, ingredients, quantity, total_price_cents, order_id }) {
+  constructor({ id, product, ingredients, quantity, total_price_cents, order_id, evaluation }) {
     this.id = id;
     this.orderId = order_id;
     this.product = new Product(product);
     this.quantity = quantity;
     this.ingredients = ingredients;
     this.totalPriceCents = total_price_cents;
+    this.evaluation = new Evaluation(evaluation);
   }
 
   get productOrderIngredients() {
