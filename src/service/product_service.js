3e60c7826fd27947367341769e82c9f09c83ff70
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-10-05 18:35:09 -0300

    return promises

--- src/service/product_service.js
+++ src/service/product_service.js
@@ -1,18 +1,18 @@
 import request from '../lib/request';
 
-const allProductsByRestaurant = (dispatch, { restaurantID }) => {
+const allProductsByRestaurant = (dispatch, { restaurantID }) => new Promise((resolve) => {
   request.get(`restaurants/${restaurantID}/products`)
     .then((products) => {
-      dispatch('allProducts', products);
+      dispatch('allProducts', products).then(() => resolve(products));
     });
-};
+});
 
-const productByRestaurant = (dispatch, { restaurantID, productID }) => {
+const productByRestaurant = (dispatch, { restaurantID, productID }) => new Promise((resolve) => {
   request.get(`restaurants/${restaurantID}/products/${productID}`)
-  .then((product) => {
-    dispatch('currentProduct', product);
-  });
-};
+    .then((product) => {
+      dispatch('currentProduct', product).then(() => resolve(product));
+    });
+});
 
 export default {
   allProductsByRestaurant,
